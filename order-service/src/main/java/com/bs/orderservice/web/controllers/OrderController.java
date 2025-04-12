7152771276af09d825d925aeb327c8d9ca726954
Vicktor007 <vicktord007@gmail.com>
2025-04-12 20:32:41 +0100

    order service in progress

--- order-service/src/main/java/com/bs/orderservice/web/controllers/OrderController.java
+++ order-service/src/main/java/com/bs/orderservice/web/controllers/OrderController.java
@@ -4,6 +4,7 @@ import com.bs.orderservice.domain.OrderService;
 import com.bs.orderservice.domain.SecurityService;
 import com.bs.orderservice.domain.records.OrderRequest;
 import com.bs.orderservice.domain.records.OrderResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
+@SecurityRequirement(name = "security_auth")
 class OrderController {
     private static final Logger log = LoggerFactory.getLogger(OrderController.class);
     private final OrderService orderService;
