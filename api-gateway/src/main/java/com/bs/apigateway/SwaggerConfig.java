a915f02b2bb5f25c38ed8916a565196f85b89a86
Vicktor007 <vicktord007@gmail.com>
2025-04-12 20:02:39 +0100

    api-gateway done: github actions wll still fail

---
+++ api-gateway/src/main/java/com/bs/apigateway/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.bs.apigateway;
+
+import jakarta.annotation.PostConstruct;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
+
+@Configuration
+ class SwaggerConfig {
+
+    private final RouteDefinitionLocator routeDefinitionLocator;
+    private final SwaggerUiConfigProperties swaggerUiConfigProperties;
+
+    public SwaggerConfig(RouteDefinitionLocator routeDefinitionLocator, SwaggerUiConfigProperties swaggerUiConfigProperties) {
+        this.routeDefinitionLocator = routeDefinitionLocator;
+        this.swaggerUiConfigProperties = swaggerUiConfigProperties;
+    }
+
+    @PostConstruct
+    public void init() {
+        List<RouteDefinition> routeDefinitions = routeDefinitionLocator.getRouteDefinitions()
+                .collectList().block();
+        Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
+        routeDefinitions.stream()
+                .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
+                .forEach(routeDefinition -> {
+                    String name = routeDefinition.getId().replaceAll("-service", "");
+                    AbstractSwaggerUiConfigProperties.SwaggerUrl url = new AbstractSwaggerUiConfigProperties.SwaggerUrl(
+                            name, DEFAULT_API_DOCS_URL + "/" + name, null
+                    );
+                    urls.add(url);
+                });
+        swaggerUiConfigProperties.setUrls(urls);
+    }
+}
